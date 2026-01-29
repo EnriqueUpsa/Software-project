7e367429d5329cc928996a18ec761a771e9149fd
EnriqueUpsa <egarciabe.inf@upsa.es>
2026-01-30 00:50:04 +0100

    Add Dog and Cat classes extending Animal

--- src/main/java/model/Animal.java
+++ src/main/java/model/Animal.java
@@ -1,4 +1,15 @@
 package model;
 
+import java.time.LocalDate;
+
 public abstract class Animal {
+    protected String microchipId;
+    protected String breed;
+    protected LocalDate intakeDate;
+
+    public Animal(String microchipId, String breed, LocalDate intakeDate) {
+        this.microchipId = microchipId;
+        this.breed = breed;
+        this.intakeDate = intakeDate;
+    }
 }
