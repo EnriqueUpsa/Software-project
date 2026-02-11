cabe5540e7039e1004adfbe1ce144f85e91b6d58
EnriqueUpsa <egarciabe.inf@upsa.es>
2026-02-11 16:02:35 +0100

    Add HealthRecordDAO interface

---
+++ src/main/java/dao/HealthRecordDAO.java
@@ -0,0 +1,11 @@
+package dao;
+
+import model.HealthRecord;
+
+import java.util.List;
+
+public interface HealthRecordDAO {
+    void save(HealthRecord record);
+
+    List<HealthRecord> findByMicrochipId(String microchipId);
+}
