0de8123c2d0df2efeedaac1c9f7f0854b7db63bd
EnriqueUpsa <egarciabe.inf@upsa.es>
2026-02-11 16:49:35 +0100

    Add status-aware constructors for Dog and Cat

--- src/main/java/model/Cat.java
+++ src/main/java/model/Cat.java
@@ -6,4 +6,9 @@ public class Cat extends Animal{
     public Cat(String microchipId, String breed, LocalDate intakeDate) {
         super(microchipId, breed, intakeDate);
     }
+
+    public Cat(String microchipId, String breed, LocalDate intakeDate,
+               Status status) {
+        super(microchipId, breed, intakeDate, status);
+    }
 }
--- src/main/java/model/Dog.java
+++ src/main/java/model/Dog.java
@@ -5,4 +5,9 @@ public class Dog extends Animal{
     public Dog(String microchipId, String breed, LocalDate intakeDate) {
         super(microchipId, breed, intakeDate);
     }
+
+    public Dog(String microchipId, String breed, LocalDate intakeDate,
+               Status status) {
+        super(microchipId, breed, intakeDate, status);
+    }
 }
