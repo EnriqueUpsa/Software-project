dbb2d6a65e211f81051e1e1fff58997c0c1b19b6
EnriqueUpsa <egarciabe.inf@upsa.es>
2026-02-11 16:06:50 +0100

    Add InMemoryHealthRecordDAO

---
+++ src/main/java/dao/InMemoryHealthRecordDAO.java
@@ -0,0 +1,23 @@
+package dao;
+
+import model.HealthRecord;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class InMemoryHealthRecordDAO implements HealthRecordDAO {
+    private final List<HealthRecord> records = new ArrayList<>();
+
+    @Override
+    public void save(HealthRecord record) {
+        records.add(record);
+    }
+
+    @Override
+    public List<HealthRecord> findByMicrochipId(String microchipId) {
+        return records.stream()
+                .filter(record -> record.getMicrochipId().equals(microchipId))
+                .collect(Collectors.toList());
+    }
+}
